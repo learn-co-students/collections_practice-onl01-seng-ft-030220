8fcb0838eb80c24e1d289e97f2caf9adc2f65fb7
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 17:33:09 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,16 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4.5
+  describe '#swap_elements_from_to' do
+    it 'swap the specified elements of an array' do
+      array = ["blake", "ashley", "scott", "chris"]
+      return_array = ["scott", "ashley", "blake", "chris"]
+
+      expect(swap_elements_from_to(array, 0, 2)).to eq(return_array)
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
